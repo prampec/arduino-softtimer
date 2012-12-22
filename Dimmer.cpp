50bf6e1e69a7a64ccb22e0727b3aa9a17aac91fe
Balázs Kelemen <10376327+prampec@users.noreply.github.com>
2012-12-22 21:31:26 +0000

    Adding Dimmer task.

---
+++ Dimmer.cpp
@@ -0,0 +1,98 @@
+/**
+ * File: Dimmer.cpp
+ * Description:
+ * SoftTimer library is a lightweight but effective event based timeshare solution for Arduino.
+ *
+ * Author: Balazs Kelemen
+ * Contact: prampec+arduino@gmail.com
+ * Copyright: 2012 Balazs Kelemen
+ * Copying permission statement:
+    This file is part of SoftTimer.
+
+    SoftTimer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "SoftTimer.h"
+#include "Dimmer.h"
+
+Dimmer::Dimmer(SoftPwmTask* pwm, int frequencyMs) : Task(10, &(Dimmer::step))
+{
+  this->_pwm = pwm;
+  this->direction = DIMMER_DIRECTION_HIGH;
+  this->value = 0;
+  this->_pwm->analogWrite((byte)this->value);
+  this->stepCount = DEFAULT_STEP_COUNT;
+
+  this->setFrequency(frequencyMs);
+}
+
+
+void Dimmer::startPulsate() {
+  this->stopOnLimit = false;
+  SoftTimer.add(this->_pwm);
+  SoftTimer.add(this);
+}
+
+void Dimmer::hold() {
+  SoftTimer.remove(this);
+}
+
+void Dimmer::off() {
+  this->hold();
+  this->_pwm->off();
+  SoftTimer.remove(this);
+  SoftTimer.remove(this->_pwm);
+}
+
+void Dimmer::revertDirection() {
+  this->direction *= -1;
+}
+
+void Dimmer::setFrequency(int frequencyMs) {
+  this->_stepLevel = (float)this->_pwm->upperLimit / (float)this->stepCount;
+  this->periodMicros = (float)frequencyMs * 500.0 / (float)this->stepCount;
+  
+}
+
+byte Dimmer::getUpperLimit() {
+  return this->_pwm->upperLimit;
+}
+
+
+void Dimmer::step(Task* task)
+{
+  Dimmer* dimmer = (Dimmer*)task;
+  
+  boolean isOnLimit = false;
+  
+  dimmer->value += dimmer->direction * dimmer->_stepLevel;
+  if((dimmer->direction < 0) && (dimmer->value < 0)) {
+    dimmer->value = 0;
+    dimmer->direction *= -1; // -- next time go in the other direction
+    isOnLimit = true;
+  } else if((dimmer->direction > 0) && (dimmer->value > dimmer->_pwm->upperLimit)) {
+    dimmer->value = dimmer->_pwm->upperLimit;
+    dimmer->direction *= -1; // -- next time go in the other direction
+    isOnLimit = true;
+  }
+  
+  dimmer->_pwm->analogWrite((byte)dimmer->value);
+  
+  if(isOnLimit && dimmer->stopOnLimit) {
+    SoftTimer.remove(dimmer);
+  }
+
+}
+
---
+++ examples/SoftTimer8Dimmer/SoftTimer8Dimmer.ino
@@ -0,0 +1,37 @@
+// -- Pin change interrupt
+#include <PciManager.h>
+#include <SoftTimer.h>
+#include <Debouncer.h>
+#include <SoftPwmTask.h>
+#include <Dimmer.h>
+
+#define OUT_PIN  13
+#define INPUT_PIN 3
+#define ON_OFF_FREQ_MS 1500
+#define TRESHOLD_MS 600
+
+// -- Button for input.
+Debouncer debouncer(INPUT_PIN, MODE_CLOSE_ON_PUSH, onPressed, onReleased);
+// -- Set up PWM to the out pin.
+SoftPwmTask pwm(OUT_PIN);
+Dimmer dimmer(&pwm, 1500);
+
+void setup() {
+  PciManager.registerListener(INPUT_PIN, &debouncer);
+}
+
+void onPressed() {
+  dimmer.startPulsate();
+}
+
+void onReleased(unsigned long pressTimespan) {
+  if(pressTimespan < TRESHOLD_MS) {
+    // -- It was just a fast clik, so stop on 0 or upperLimit.
+    dimmer.stopOnLimit = true;
+  } else {
+    // -- Stop immediatelly.
+    dimmer.hold();
+  }
+}
+
+
